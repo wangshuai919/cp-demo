2cb21e9dc76881595cc98a7f4d70b1147e9c4112
WangShuai <abc_billy@163.com>
2022-07-18 16:12:26 +0800

    project init

---
+++ src/test/java/com/ciccwm/cpdemo/CpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciccwm.cpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
